bb4539d5cd1cea2efe48401e72c2cbb2e9665392
unknown <hasinadilruba2021@gmail.com>
2022-12-10 21:08:07 +0100

    private route created

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,20 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider';
+import toast from 'react-hot-toast';
 
 const SignUp = () => {
   const {register, formState: { errors }, handleSubmit} = useForm();
-  const {createUser} = useContext(AuthContext);
+  const {createUser, updateUser} = useContext(AuthContext);
+  const [signUpError, setSignUpError] = useState('');
 
   const handleSignUp = (data) =>{
     console.log(data);
+    setSignUpError('');
     createUser(data.email, data.password)
     .then(result =>{
        const user = result.user;
        console.log(user);
+       toast.success('User Created Successfully')
+       const userInfo = {
+        displayName: data.name
+       }
+       updateUser(userInfo)
+       .then(() =>{})
+       .catch(err => console.log(err));
     })
-    .catch(error => console.log(error))
+    .catch(error => {
+      console.log(error)
+      setSignUpError(error.message)
+    });
   }
 
   return (
@@ -59,6 +72,7 @@ const SignUp = () => {
             </div>
               <input className='btn btn-accent w-full' value="Sign Up"
               type="submit" />
+              {signUpError && <p className='text-red-600'>{signUpError}</p>}
           </form>
 
           <p className='my-2'><small>Allready have an account?</small> <Link className='text-secondary' to="/login">Please Login </Link></p> 
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile} from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
@@ -7,22 +7,32 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
    const [user, setUser] = useState(null);
+   const [loading, setLoading] = useState(true);
+
     const createUser = (email, password) =>{
+      setLoading(true);
          return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const signIn = (email, password) =>{
+      setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
    const logOut = () =>{
+    setLoading(true);
       return signOut(auth)
+   }
+
+   const updateUser = (userInfo) =>{
+        return updateProfile(user, userInfo)
    }
 
     useEffect( () =>{
       const unsubscribe = onAuthStateChanged(auth, currentUser =>{
          console.log('user observing');
          setUser(currentUser)
+         setLoading(false);
        });
 
        return () => unsubscribe();
@@ -32,7 +42,9 @@ const AuthProvider = ({children}) => {
         createUser,
         signIn,
         logOut,
-        user
+        user,
+        updateUser,
+        loading
    }
 
   return (
