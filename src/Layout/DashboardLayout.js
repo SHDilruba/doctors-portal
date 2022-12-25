fef22014c2950914cde01ce55cd9b92a72fc2252
unknown <hasinadilruba2021@gmail.com>
2022-12-25 22:55:56 +0100

    get image url by using imgbb

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider';
+import useAdmin from '../hooks/useAdmin';
 import Navbar from '../Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
+  const {user} = useContext(AuthContext);
+  const [isAdmin] = useAdmin(user?.email)
   return (
     <div>
        <Navbar></Navbar>
@@ -15,9 +19,14 @@ const DashboardLayout = () => {
     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label> 
     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
       <li><Link to="/dashboard">My Appointments</Link></li>
-      <li><Link to="/dashboard/allusers">All Users</Link></li>
-    </ul>
-  
+       {
+          isAdmin && <>
+              <li><Link to="/dashboard/allusers">All Users</Link></li>
+              <li><Link to="/dashboard/adddoctor">Add a  Doctor</Link></li>
+              <li><Link to="/dashboard/managedoctors">Manage  Doctors</Link></li>
+          </>
+       }
+    </ul>  
   </div>
 </div>
     </div>
